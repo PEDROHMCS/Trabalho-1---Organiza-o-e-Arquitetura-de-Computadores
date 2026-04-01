a0e4ca9f42b308755ff04c7679e84050cdcce4f0
PedroMrks <pedrohmcspro@gmail.com>
2026-03-31 21:40:41 -0300

    Remocao implementada

--- trab1.asm
+++ trab1.asm
@@ -1,6 +1,6 @@
 	.data
 	.align 2
-	ptr_head: .word 0 #ponteiro para o nó head (locomotiva)
+	ptr_head: .word 0 #ponteiro para o no head (locomotiva)
 	.align 2
 	next_id: .word 2 #contador para IDs
 	
@@ -14,7 +14,7 @@ main:
 	#inicializar o trem (cria a locomotiva)
 	jal ra, construtor
 	
-	# input dos vagões
+	# input dos vagï¿½es
 	
 	
 	
@@ -23,30 +23,30 @@ main:
 	ecall
 
 construtor:
-	# Alocar 12 bytes na memória dinâmica para o nó
+	# Alocar 12 bytes na memoria dinamica para o no=
 	# 4 bytes para o ID;
 	# 4 bytes para string do tipo (3 caracteres e \0);
-	# 4 bytes para o ponteiro do próximo 
+	# 4 bytes para o ponteiro do proximo 
 	addi a7, zero, 9
 	addi a0, zero, 12
 	ecall
 	
-	# definir valores padrões do nó
+	# definir valores padroes do no
 	sw zero, 0(a0) # define o ID para 0
-	sw zero, 8(a0) # define o ponteiro do próximo para 0
+	sw zero, 8(a0) # define o ponteiro do prï¿½ximo para 0
 	
-	# Definir o endereço para onde ptr_head vai apontar 
+	# Definir o endereï¿½o para onde ptr_head vai apontar 
 	la t1, ptr_head
-	sw a0, 0(t1) # ptr_head aponta para o espaço na heap que a0 guarda
+	sw a0, 0(t1) # ptr_head aponta para o espaï¿½o na heap que a0 guarda
 	
 	# Voltar para a main
 	jalr zero, 0(ra)
 		
 new_no:
 	# Argumentos esperados
-	# a0 = Tipo do vagão
+	# a0 = Tipo do vagï¿½o
 
-	add t4, zero, a0 # guarda o tipo do vagão no registrador temporário
+	add t4, zero, a0 # guarda o tipo do vagï¿½o no registrador temporï¿½rio
 	
 	# Alocar os bytes na heap
 	addi a7, zero, 9
@@ -54,38 +54,79 @@ new_no:
 	ecall
 	
 	# Ler o valor do Id atual e escrever na heap
-	la t1, next_id # t1 recebe o endereço de next_id
+	la t1, next_id # t1 recebe o endereï¿½o de next_id
 	lw t3, 0(t1) # t3 recebe o valor do id atual
 	sw t3, 0(a0) # escreve o valor do id atual nos 4 primeiros bytes
 	
 	# Incrementar valor do next_Id
-	addi t3, t3, 1 # incrementa o id para o próximo nó
+	addi t3, t3, 1 # incrementa o id para o prï¿½ximo nï¿½
 	sw t3, 0(t1) # atualiza o valor de next_id
 	
-	# Escrever o tipo do vagão na heap
-	sw t4, 4(a0) # escreve o tipo do vagão nos bytes 4 - 7
-	sw zero, 8(a0) # escreve o valor 0 nos bytes do ponteiro para o próximo vagão
+	# Escrever o tipo do vagï¿½o na heap
+	sw t4, 4(a0) # escreve o tipo do vagï¿½o nos bytes 4 - 7
+	sw zero, 8(a0) # escreve o valor 0 nos bytes do ponteiro para o prï¿½ximo vagï¿½o
 	
-	la t1, ptr_head # t1 recebe o endereço de ptr_head
-	lw t0, 0(t1) # t0 recebe o conteúdo de head
-	# programa entra na função de busca do último nó
+	la t1, ptr_head # t1 recebe o endereï¿½o de ptr_head
+	lw t0, 0(t1) # t0 recebe o conteï¿½do de head
+	# programa entra na funï¿½ï¿½o de busca do ï¿½ltimo nï¿½
 
 search_last:
-	lw t1, 8(t0) # t1 lê o ponteiro para o próximo nó do vagão atual
+	lw t1, 8(t0) # t1 lï¿½ o ponteiro para o prï¿½ximo nï¿½ do vagï¿½o atual
 	
-	# Verificar se o valor é 0 (se sim, significa que é o último vagão)
+	# Verificar se o valor ï¿½ 0 (se sim, significa que ï¿½ o ï¿½ltimo vagï¿½o)
 	beq t1, zero, link_no
 	
-	# Se não for zero, percorre a lista
-	add t0, t1, zero #t0 recebe o endereço guardado no ponteiro para o próximo vagão
+	# Se nï¿½o for zero, percorre a lista
+	add t0, t1, zero #t0 recebe o endereï¿½o guardado no ponteiro para o prï¿½ximo vagï¿½o
 	jal zero, search_last
 
 link_no:
-	# Guardar o endereço do nó atual no ponteiro do anterior
+	# Guardar o endereï¿½o do nï¿½ atual no ponteiro do anterior
 	sw a0, 8(t0)
 	
 	# Retornar para a main
 	jalr zero, 0(ra)
-
-
 	
+	
+# ================ REMOÃ‡ÃƒO ====================
+	
+remove_no:
+	add t0, a0, zero # Copia o id que queremos apagar para t0
+	
+	# t1 armazena o endereÃ§o da locomotiva
+	la t1, ptr_head
+	lw t1, 0(t1)
+	
+	# Se a lista estiver vazia t1 == 0, nÃ£o faz nada
+	beq t1, zero, fim_remove
+	
+	#t2 le o id da locomotiva, se o que o usuario digitou for da locomotiva
+	lw t2, 0(t1)
+	beq t0, t2, fim_remove
+
+remove_busca:
+	#t1 Ã© o anterior e t2 Ã© o atual
+	
+	# t2 recebe o endereÃ§o do proximo vagao
+	lw t2, 8(t1)
+	
+	# Se t2 for zero, Ã© o fim do trem e o ID nao existe
+	beq t2, zero, fim_remove
+	
+	# ID do vagÃ£o atual que esta em t2, Ã© colocado em t3
+	lw t3, 0(t2)
+	
+	# Se o id a ser apagado for igual ao atual, executamos a remocao
+	beq t3, t0, remove_executa
+	
+	# caso nao, continuamos na busca recursiva, e t1(anterior), passa a ser o t2(atual)
+	add t1, t2, zero
+	jal zero, remove_busca
+
+remove_executa:
+	# t4, armazena o endereco do vagao depois do que serÃ¡ removido agora
+	lw t4, 8(t2)
+	sw t4, 8(t1)
+
+fim_remove:
+	jalr zero, 0(ra)
